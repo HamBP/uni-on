5d75745d6e287a13a476dbf295d2d0b9d541fafd
algosketch <sjy_9484@naver.com>
2021-09-03 09:39:06 +0900

    react init setting

---
+++ src/components/board/Board.js
@@ -0,0 +1,13 @@
+import "./Board.css";
+
+function Board() {
+  return (
+    <div className="board">
+      <div className="container">
+        123
+      </div>
+    </div>
+  );
+}
+
+export default Board;
