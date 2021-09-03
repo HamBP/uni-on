865f52c8e3296d78ead30602c7fd18f626bbfbcc
algosketch <sjy_9484@naver.com>
2021-09-04 01:34:05 +0900

    add reactive test

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -11,44 +11,46 @@ function Board() {
           <h3>Noto Sans KR</h3>
           <a href="#">글쓰기</a>
         </div>
-        <div className="box">
-          <table>
-            <tr>
-              <th>ID</th>
-              <th colSpan="7">제목</th>
-              <th>작성자</th>
-              <th>작성일</th>
-              <th>조회</th>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td colSpan="7">로렘입숨</td>
-              <td>익명</td>
-              <td>2021.09.03.</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td colSpan="7">로렘입숨</td>
-              <td>익명</td>
-              <td>2021.09.03.</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </table>
+        <div className="box-padding">
+          <div className="box">
+            <table>
+              <tr className="mobile-none">
+                <th>ID</th>
+                <th>제목</th>
+                <th>작성자</th>
+                <th>작성일</th>
+                <th>조회</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>로렘입숨</td>
+                <td>익명</td>
+                <td>2021.09.03.</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>로렘입숨</td>
+                <td>익명</td>
+                <td>2021.09.03.</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>
