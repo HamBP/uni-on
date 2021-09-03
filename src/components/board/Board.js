ae41f8760b513b1ba1ac14b4af0994381c25f794
algosketch <sjy_9484@naver.com>
2021-09-04 08:47:01 +0900

    add function to insert in board

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -3,6 +3,26 @@ import Header from "../header/Header";
 import {Link} from "react-router-dom";
 
 function Board() {
+  const setArticle = () => {
+    const whereToInsert = document.getElementById("articles");
+
+    const li = document.createElement("li");
+    li.className = "article";
+
+    const p1 = document.createElement("p");
+    p1.innerText = "test";
+    p1.className = "article-title"
+
+    const p2 = document.createElement("p");
+    p2.innerText = "t e s t";
+    p2.className = "article-info"
+
+    li.append(p1);
+    li.append(p2);
+
+    whereToInsert.insertBefore(li, whereToInsert.firstChild);
+  }
+
   return (
     <div className="board">
       <Header/>
@@ -11,13 +31,13 @@ function Board() {
         <div className="header">
           <h3>Noto Sans KR</h3>
           <div className="menu">
-            <span className="search"/>
+            <span className="search" onClick={setArticle}/>
             <Link to="/post" className="create">글쓰기</Link>
           </div>
         </div>
         <div className="box-padding">
           <div className="box">
-            <ul>
+            <ul id="articles">
               <li className="article">
                 <p className="article-title">로렘입숨1</p>
                 <p className="article-info">익명 2021.09.03. 조회 0</p>
