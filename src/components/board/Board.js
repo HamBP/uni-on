338df8d5a5456c40def5dcd2cd67da79d57367f4
algosketch <sjy_9484@naver.com>
2021-09-03 10:31:18 +0900

    add layout

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -5,9 +5,51 @@ function Board() {
   return (
     <div className="board">
       <Header/>
-      <span className="banner"/>
       <div className="container">
-        123
+        <span className="banner">원래 이미지 들어가야 됨</span>
+        <div className="header">
+          <h3>Noto Sans KR</h3>
+          <a href="#">글쓰기</a>
+        </div>
+        <div className="box">
+          <table>
+            <tr>
+              <th>ID</th>
+              <th colSpan="7">제목</th>
+              <th>작성자</th>
+              <th>작성일</th>
+              <th>조회</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td colSpan="7">로렘입숨</td>
+              <td>익명</td>
+              <td>2021.09.03.</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td colSpan="7">로렘입숨</td>
+              <td>익명</td>
+              <td>2021.09.03.</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   );
