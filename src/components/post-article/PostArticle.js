b6bd07794c837faae50eac5f94c7a692f17225d3
algosketch <sjy_9484@naver.com>
2021-09-04 09:35:53 +0900

    add function to post article to server

--- src/components/post-article/PostArticle.js
+++ src/components/post-article/PostArticle.js
@@ -1,16 +1,31 @@
 import "./PostArticle.css";
 import {Link} from "react-router-dom";
+import axios from "axios";
+import network from "../../global/network";
 
 function PostArticle() {
+  const postArticle = () => {
+    const title = document.getElementById("title").value;
+    const content = document.getElementById("content").value;
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+
+    axios.post(network.baseURL + "articles", formData)
+      .then(res => {
+        console.log("요청");
+      })
+  }
+
   return (
     <div className="post-article">
       <div className="header">
         <Link to="/" className="undo"/>
-        <button className="submit">등록</button>
+        <button className="submit" onClick={postArticle}>등록</button>
       </div>
       <div className="box">
-        <input type="text" className="title"/>
-        <textarea name="" id="" cols="30" rows="10" className="content"/>
+        <input id="title" type="text" className="title"/>
+        <textarea name="" id="content" cols="30" rows="10" className="content"/>
       </div>
     </div>
   );
